c64baa9f36e1078c3716b61f46eb4f22a1695a4a
Tanvirul Hasan <tanvirulhasanedesh@gmail.com>
2024-11-19 11:33:15 +0600

    removed used code

--- app/(pages)/resume/page.tsx
+++ app/(pages)/resume/page.tsx
@@ -1,13 +1,5 @@
 "use client";
-import {
-  FaHtml5,
-  FaCss3,
-  FaJs,
-  FaReact,
-  FaFigma,
-  FaNodeJs,
-} from "react-icons/fa";
-import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+import { FaHtml5, FaCss3 } from "react-icons/fa";
 
 const about = {
   title: "About me",
