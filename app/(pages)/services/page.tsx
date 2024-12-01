2d65adb0811b70db87f185085ec753bff3736b4c
Tanvirul Hasan <tanvirulhasanedesh@gmail.com>
2024-12-01 11:08:33 +0600

    update service page

--- app/(pages)/services/page.tsx
+++ app/(pages)/services/page.tsx
@@ -1,31 +1,69 @@
 "use client";
 import { motion } from "motion/react";
+import axios, { HttpStatusCode } from "axios";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { BsArrowDownRight } from "react-icons/bs";
-const services = [
+
+type Service = {
+  success: boolean;
+  statusCode: HttpStatusCode;
+  message: string;
+  error: object;
+  data: [
+    {
+      id: number;
+      name: string;
+      description: string;
+    }
+  ];
+};
+const data = [
   {
-    num: "01",
-    title: "Web Application",
+    id: "01",
+    name: "Web Application",
     description:
       "Develop robust online applications to suit your business needs",
-    href: "",
   },
   {
-    num: "02",
-    title: "Mobile Application",
+    id: "02",
+    name: "Mobile Application",
     description:
       "Developing innovative and native mobile apps for Android, iOS platforms",
-    href: "",
   },
   {
-    num: "03",
-    title: "E-Commerce",
+    id: "03",
+    name: "E-commerce",
     description:
       "Provide ultimate e-commerce solution as well as customizations for your e-commerce business",
-    href: "",
   },
 ];
 const Services = () => {
+  const [services, setServices] = useState<Service>();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const baseUrl = "https://localhost:7013/api/service/GetAll";
+
+  const fetchData = async () => {
+    await axios
+      .get(baseUrl)
+      .then((res) => {
+        return setLoading(true), setServices(res.data);
+      })
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  };
+  useEffect(() => {
+    // fetchData();
+  }, []);
+
+  console.log("data", services);
+  console.log("error", error);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
   return (
     <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
       <div className="container mx-auto">
@@ -35,21 +73,22 @@ const Services = () => {
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
           }}
-          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+          className="grid grid-cols-1 xl:grid-cols-2 gap-[60px]"
         >
-          {services.map((item, index) => {
+          {data.map((item, index) => {
             return (
               <div
                 key={index}
                 className="flex-1 flex flex-col justify-center gap-6 group"
               >
                 {/* top */}
+
                 <div className="flex w-full justify-between items-center">
                   <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
-                    {item.num}
+                    {item.id}
                   </div>
                   <Link
-                    href={item.href}
+                    href={""}
                     className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
                   >
                     <BsArrowDownRight className="text-primary text-3xl" />
@@ -57,10 +96,10 @@ const Services = () => {
                 </div>
                 {/* title */}
                 <h1 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
-                  {item.title}
+                  {item.name}
                 </h1>
                 {/* description */}
-                <p>{item.description}</p>
+                <p className="">{item.description}</p>
                 {/* border */}
                 <div className="border-b border-white/20 w-full"></div>
               </div>
